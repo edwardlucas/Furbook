1fee4d1789c1632d71a38845f6c072d7c5fa3094
MianNiu <fangdh@gmail.com>
2017-09-12 20:35:23 +0800

    Returning views. P28 定义了about.php作为view，route中使用了with来传递猫的数量

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('cats', function() {
 Route::get('cats/{id}', function($id) {
     return sprintf('Cat #%s', $id);
 })->where('id', '[0-9]+');
+
+Route::get('about', function() {
+    return view('about')->with('number_of_cats', 9000);
+});
