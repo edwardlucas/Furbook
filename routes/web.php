20c289ebf79cf2b3b56415bcc9a61b88a64b21f1
MianNiu <fangdh@gmail.com>
2017-09-12 19:53:42 +0800

    Writing the first routes. P26

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
+Route::get('/', function() {
+    return 'All cats';
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('cats/{id}', function($id) {
+    return sprintf('Cat #%s', $id);
 });
