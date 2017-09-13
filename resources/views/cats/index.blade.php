9b1376cbc87c6bb337cc4d43658af2fe25280b3d
MianNiu <fangdh@gmail.com>
2017-09-13 16:48:51 +0800

    增加了以下的route和view cats                   -----> cats/index.blade.php cats/breeds/{name}     -----> cats/index.blade.php cats/{id}              -----> cats/show.blade.php
    另外修改了书中代码用@forelse @empty代替了原先的@foreach，在没有猫时输出提示。
    
    问题：cats/{id} 访问不存在的猫时会在view那里出错。

---
+++ resources/views/cats/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.master')
+@section('header')
+    @if (isset($breed))
+        <a href="{{ url('/') }}">Back to the overview</a>
+    @endif
+    <h2>
+        All @if (isset($breed)){{ $breed->name }}@endif Cats
+        <a href="{{ url('cats/create') }}" class="btn btn-primary pull-right">
+            Add a new cat
+        </a>
+    </h2>
+@stop {{--you end it with @endsection (or its alias @stop )--}}
+
+@section('content')
+    {{--
+@forelse is a @foreach that also allows you to program in a fallback if the object
+you’re iterating over is empty. We saw it in action at the start of this chapter
+
+@forelse ($talks as $talk)
+• {{ $talk->title }} ({{ $talk->length }} minutes)<br>
+@empty
+No talks this day.
+@endforelse
+    --}}
+
+    {{--下面用@forelse来替代书中代码，测试没有猫时是否能够出现“还没有任何一只猫”这样的提示
+    @foreach ($cats as $cat)
+        <div class="cat">
+            <a href="{{ url('cats/'.$cat->id) }}">
+                <strong>{{ $cat->name }}</strong> - {{ $cat->breed->name }}
+            </a>
+        </div>
+    @endforeach
+    --}}
+    @forelse ($cats as $cat)
+        <div class="cat">
+            <a href="{{ url('cats/'.$cat->id) }}">
+                <strong>{{ $cat->name }}</strong> - {{ $cat->breed->name }}
+            </a>
+        </div>
+    @empty
+        <div class="alert">
+            还没有任何一只猫。
+        </div>
+    @endforelse
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,28 @@
 <?php
-Route::get('/', function() {
+Route::get('/', function () {
     return redirect('cats');
 });
 
-Route::get('cats', function() {
-    return 'All cats';
+Route::get('cats', function () {
+    $cats = Furbook\Cat::all();
+    return view('cats.index')->with('cats', $cats);
 });
 
-Route::get('cats/{id}', function($id) {
-    return sprintf('Cat #%s', $id);
+Route::get('cats/{id}', function ($id) {
+    $cat = Furbook\Cat::find($id);
+    return view('cats.show')->with('cat', $cat);
 })->where('id', '[0-9]+');
 
-Route::get('about', function() {
+Route::get('cats/breeds/{name}', function ($name) {
+    $breed = Furbook\Breed::with('cats')
+        ->whereName($name)
+        ->first();
+    return view('cats.index')
+        ->with('breed', $breed)
+        ->with('cats', $breed->cats);
+});
+
+Route::get('about', function () {
     return view('about')->with('number_of_cats', 9000);
 });
+
